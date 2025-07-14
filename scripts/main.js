687e55a19c1a8f7b83bb859fe8eb86a97e152166
Seraphalim4404 <nickolosebastian@gmail.com>
2025-07-14 14:27:52 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,16 @@
 Blocks.scatter.ammoTypes.get(Items.lead).splashDamage = 10 * 1.6;
-Blocks.scatter.ammoTypes.new(Items.blastCompound).splashDamage = 10 * 3;
+Blocks.scatter.ammoTypes Items.blastCompound, new FlakBulletType(5f, 3){ 
+    lifetime = 60f;
+    ammoMultiplier = 5f;
+    shootEffect = Fx.shootSmall;
+    reloadMultiplier = 0.5f;
+    width = 6f;
+    height = 8f;
+    hitEffect = Fx.flakExplosion;
+    splashDamage = 22f * 1.5f;
+    splashDamageRadius = 24f;
+
+    frontColor = Pal.blastAmmoFront;
+    backColor = hitColor = Pal.blastAmmoBack;
+    despawnEffect = Fx.hitBulletColor;
+}
