fa506cb7b6cab6717b602a9ff844030fec78698b
Seraphalim4404 <nickolosebastian@gmail.com>
2025-08-17 22:20:33 +0800

    Update and rename scripts/main.js to scripts/blocks/turrets.js

---
+++ scripts/blocks/turrets.js
@@ -0,0 +1,4 @@
+Blocks.spectre.coolant.ConsumeCoolant = 20 / 60f;
+Blocks.spectre.coolantMultiplier = 1.5f;
+
+Blocks.meltdown.coolant.ConsumeCoolant = 50 / 60f;
--- scripts/main.js
+++
@@ -1,2 +0,0 @@
-Blocks.scatter.ammoTypes.get(Items.lead).splashDamage = 10 * 1.6;
-Blocks.scatter.ammoTypes.get(Items.lead).splashDamageRadius = 8 * 1.2;
