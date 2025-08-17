67490220423211e05e3765762ac42eae4d9b8fb2
Seraphalim4404 <nickolosebastian@gmail.com>
2025-08-17 22:40:02 +0800

    Update turrets.js

--- scripts/blocks/turrets.js
+++ scripts/blocks/turrets.js
@@ -1,4 +1,4 @@
-Blocks.spectre.(consume).coolant.ConsumeCoolant = 0.33333333333
+Blocks.spectre.consume.coolant.ConsumeCoolant = 0.33333333333
 Blocks.spectre.coolantMultiplier = 1.5;
 
 Blocks.meltdown.ConsumeCoolant = 50 / 60;
