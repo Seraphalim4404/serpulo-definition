f70380deae602c229f345a5e59e8bcf4f7197022
Seraphalim4404 <nickolosebastian@gmail.com>
2025-08-20 17:26:49 +0800

    Create Units.js

---
+++ scripts/Units.js
@@ -0,0 +1,4 @@
+UnitTypes.reign.weapons.get(0)bullet.shots = 4;
+UnitTypes.reign.weapons.get(0)bullet.shotDelay = 3;
+UnitTypes.reign.weapons.get(0)bullet.reload = 15;
+UnitTypes.reign.weapons.get(0)bullet.damage = 85;
