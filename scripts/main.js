bd75110aeae22809d25a5447e0accf96fedd4b7f
Seraphalim4404 <nickolosebastian@gmail.com>
2025-08-20 17:37:11 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,1 @@
-UnitTypes.reign.weapons.get(0).shots = 4;
-UnitTypes.reign.weapons.get(0).shotDelay = 3;
-UnitTypes.reign.weapons.get(0).reload = 15;
-UnitTypes.reign.weapons.get(0).bullet.damage = 85;
+// testing
