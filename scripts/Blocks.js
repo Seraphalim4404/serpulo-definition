1dcb580fb9691e868a80a1d7d984e09e38fad107
Seraphalim4404 <nickolosebastian@gmail.com>
2025-08-20 15:16:39 +0800

    Delete scripts/Blocks.js

--- scripts/Blocks.js
+++
@@ -1,6 +0,0 @@
-Blocks.spectre.ammoTypes.get(Items.graphite).frontColor = Pal.graphiteAmmoFront;
-Blocks.spectre.ammoTypes.get(Items.graphite).backColor = Pal.graphiteAmmoBack;
-Blocks.spectre.ammoTypes.get(Items.graphite).hitColor = Pal.graphiteAmmoBack;
-Blocks.spectre.ammoTypes.get(Items.graphite).trailColor = Pal.graphiteAmmoBack;
-Blocks.spectre.ammoTypes.get(Items.graphite).hitEffect = Fx.hitBulletColor;
-Blocks.spectre.ammoTypes.get(Items.graphite).despawnEffect = Fx.hitBulletColor;
